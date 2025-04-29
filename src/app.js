11027f41c17ce159454eec0138b3284825a9af5e
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-04-29 11:52:11 +0200

    importar imagenes de las fichas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
+import "./assets/img/rock.png";
+import "./assets/img/paper.png";
+import "./assets/img/scissors.png";
+import "./assets/img/lizard.png";
+import "./assets/img/spock.png";
 import "./assets/img/4geeks.ico";
+
 
 window.onload = function () {
 
